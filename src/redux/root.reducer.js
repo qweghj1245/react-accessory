66a023736cf477b55a83b3c7a8593fea93dbf9d2
mazusu <qweghj1245@gmail.com>
2019-12-18 23:36:12 +0800

    init

---
+++ src/redux/root.reducer.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: [], // no persisted
+  whiteList: [], // persisted
+}
+
+const rootReducer = combineReducers({
+  
+});
+
+export default persistReducer(persistConfig, rootReducer);
---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
+import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from './root.reducer';
+import rootSaga from './root.saga';
+const sagaMiddleware = createSagaMiddleware();
+
+let middleware = [sagaMiddleware, logger];
+const store = createStore(rootReducer, applyMiddleware(...middleware));
+const persist = persistStore(store)
+
+sagaMiddleware.run(rootSaga);
+
+export { store, persist };
