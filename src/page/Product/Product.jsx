54d812cc799a7ec0ac8c91fe4a3a8ff0a8ed1c01
mazusu <qweghj1245@gmail.com>
2020-02-23 01:20:39 +0800

    fix loading fail

--- src/page/Product/Product.jsx
+++ src/page/Product/Product.jsx
@@ -15,6 +15,7 @@ import { selectAProduct } from '../../redux/product/product.selector';
 const Product = ({ history }) => {
   const dispatch = useDispatch();
   const product = useSelector(selectAProduct);
+  const isLoading = useSelector(state => state.product.isLoading);
   const user = useSelector(state => state.user.loginUser);
   const optionModel = useRef(null);
 
@@ -51,59 +52,60 @@ const Product = ({ history }) => {
   return (
     <React.Fragment>
       {
-        <FlexWrapper>
-          <Wrapper>
-            <ImageWrapper src={product.photos[0]} width={385} />
-            <ImageGroup>
+        isLoading ? <Skeleton /> :
+          <FlexWrapper>
+            <Wrapper>
+              <ImageWrapper src={product.photos[0]} width={385} />
+              <ImageGroup>
+                {
+                  product.photos.length > 1 ?
+                    product.photos.map((image, idx) => idx !== 0 ?
+                      <Image key={image} src={image} width='100' /> : null) : null
+                }
+              </ImageGroup>
+            </Wrapper>
+            <Wrapper width={322} ml={101}>
+              <Title>{product.name}</Title>
+              <Price>NT${product.price}</Price>
+              <Msg>{product.description}</Msg>
               {
-                product.photos.length > 1 ?
-                  product.photos.map((image, idx) => idx !== 0 ?
-                    <Image key={image} src={image} width='100' /> : null) : null
+                product.size.length ?
+                  <Group mb={33}>
+                    <NormalText>尺寸</NormalText>
+                    <NormalText ml={0}>{sizeString(product.size)}</NormalText>
+                  </Group> : null
               }
-            </ImageGroup>
-          </Wrapper>
-          <Wrapper width={322} ml={101}>
-            <Title>{product.name}</Title>
-            <Price>NT${product.price}</Price>
-            <Msg>{product.description}</Msg>
-            {
-              product.size.length ?
-                <Group mb={33}>
-                  <NormalText>尺寸</NormalText>
-                  <NormalText ml={0}>{sizeString(product.size)}</NormalText>
-                </Group> : null
-            }
-            {
-              product.colors.length ?
-                <Group mb={30}>
-                  <NormalText>顏色</NormalText>
-                  <Flex>
-                    {
-                      product.colors.map((color, idx) =>
-                        <ColorCircle key={color} bg={color} mr={idx === product.colors.length - 1 ? null : 20} isActive />)
-                    }
-                  </Flex>
-                </Group> : null
-            }
-            {
-              product.options.length ?
-                <Group mb={35}>
-                  <NormalText>選項</NormalText>
-                  <BaseSelect options={product.options.map(item => {
-                    return { label: item, value: item }
-                  })} change={e => optionModel.current = e} />
-                </Group> : null
-            }
-            <Group mb={50}>
-              <NormalText>數量</NormalText>
-              <AddCount total={product.quantity} />
-            </Group>
-            <Group>
-              <BaseButton color='light-brown' padding='8px 53px' mr='30'>加入購物車</BaseButton>
-              <CollectButton product={product} user={user} collect={setCollect} />
-            </Group>
-          </Wrapper>
-        </FlexWrapper>
+              {
+                product.colors.length ?
+                  <Group mb={30}>
+                    <NormalText>顏色</NormalText>
+                    <Flex>
+                      {
+                        product.colors.map((color, idx) =>
+                          <ColorCircle key={color} bg={color} mr={idx === product.colors.length - 1 ? null : 20} isActive />)
+                      }
+                    </Flex>
+                  </Group> : null
+              }
+              {
+                product.options.length ?
+                  <Group mb={35}>
+                    <NormalText>選項</NormalText>
+                    <BaseSelect options={product.options.map(item => {
+                      return { label: item, value: item }
+                    })} change={e => optionModel.current = e} />
+                  </Group> : null
+              }
+              <Group mb={50}>
+                <NormalText>數量</NormalText>
+                <AddCount total={product.quantity} />
+              </Group>
+              <Group>
+                <BaseButton color='light-brown' padding='8px 53px' mr='30'>加入購物車</BaseButton>
+                <CollectButton product={product} user={user} collect={setCollect} />
+              </Group>
+            </Wrapper>
+          </FlexWrapper>
       }
     </React.Fragment>
   )
